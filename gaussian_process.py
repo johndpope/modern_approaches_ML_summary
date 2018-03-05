4c2e7c58a1d1977c0eacc88a89f5a77409b757cf
Pablo Ruiz Ruiz <pablo.ruizruiz@list.lu>
2018-03-05 10:35:17 +0100

    amplified Gaussians

--- gaussian_process.py
+++ gaussian_process.py
@@ -20,13 +20,13 @@ import matplotlib.ticker as ticker
 path_to_data = './windows_ts.h5'
 
 dataset = pd.read_hdf(path_to_data, 'dataset')
-gp_trainset = pd.read_hdf(path_to_data, 'trainset')
-gp_testset = pd.read_hdf(path_to_data, 'testset')
+trainset = pd.read_hdf(path_to_data, 'trainset')
+testset = pd.read_hdf(path_to_data, 'testset')
 
 train_windows = pd.read_hdf(path_to_data, 'train_windows')
 test_windows = pd.read_hdf(path_to_data, 'test_windows')
-train_windows_gp_inc = pd.read_hdf(path_to_data, 'train_windows_y_inc')
-test_windows_gp_inc = pd.read_hdf(path_to_data, 'test_windows_y_inc')
+train_windows_y_inc = pd.read_hdf(path_to_data, 'train_windows_y_inc')
+test_windows_y_inc = pd.read_hdf(path_to_data, 'test_windows_y_inc')
 
 # Bring here the window used
 w = 5
@@ -64,15 +64,14 @@ trainY = train_windows.values[:,-1]
 testX = test_windows.values[:,:-1]
 testY = test_windows.values[:,-1]
 
-if 'gp_inc' not in locals(): del gp_inc
+if 'gp' in locals(): del gp_inc
 t0 = time.time()
 gp.fit(train_windows.values[:,:-1], train_windows.values[:,-1])
 tF = time.time()
 
-X = np.concatenate((trainX, testX))
-
 # Predict
-gp_y_pred, gp_y_std = gp.predict(X, return_std=True)
+gp_y_fit = gp.predict(train_windows.values[:,:-1], return_std=False)
+gp_y_pred, gp_y_std = gp.predict(test_windows.iloc[:,:-1], return_std=True)
 
 gp_y_up = (gp_y_pred + 1.96 * gp_y_std).reshape(-1,)
 gp_y_lw = (gp_y_pred - 1.96 * gp_y_std).reshape(-1,)
@@ -80,31 +79,68 @@ gp_y_pred = gp_y_pred.reshape(-1,1)
 
 
 # Calculating Errors
-gp_residuals = gp_y_pred[len(train_windows):] - gp_testset.iloc[5:,-1].values.reshape(-1,1)
+gp_residuals = gp_y_pred - testset.iloc[5:,-1].values.reshape(-1,1)
 gp_rmse = np.sqrt(np.sum(np.power(gp_residuals,2)) / len(gp_residuals))
 print('RMSE = %f' % gp_rmse)
 print('Time to train %.2f' % (tF - t0))
+
+
+# Plot predictions
+f, ax = plt.subplots(1, figsize=(20,6))
+plt.suptitle('Actual vs Predicted - MLR with previous t values' , fontsize=20)
+plt.title('RMSE = %.2f' % gp_rmse, fontsize = 18)
+plt.grid(color='green', linewidth=0.5, alpha=0.5)
+
+plt.scatter(testset.index, testset.y, color='black', s=10)
+plt.plot(testset.index, testset.y, color='b', label='Real Test')
+plt.plot(testset.index[w:], gp_y_pred, color='r', label='Predicted Test')
+
+ax.yaxis.set_major_locator(ticker.MultipleLocator(5))
+ax.yaxis.set_minor_locator(ticker.MultipleLocator(1))
+plt.xlabel('Time')
+plt.ylabel('Response')
+plt.legend()
+plt.show()
+
+
+# Complete Plot
+f, (ax1, ax2) = plt.subplots(2,1, figsize=(20,6))
+plt.suptitle('Actual vs Predicted - Gaussian Process' , fontsize=20)
+ax1.grid(color='green', linewidth=0.5, alpha=0.5)
+ax2.grid(color='green', linewidth=0.5, alpha=0.5)
+
+ax1.plot(trainset.index[w:], train_windows['y'], color='b', label='Real Train')
+ax1.plot(trainset.index[w:], gp_y_fit, color='r',
+         linewidth=0.8, alpha=0.8, label='Predicted Train')
+ax1.legend()
+ax1.set_title('Trainset')
+
+ax2.plot(testset.index[w:], test_windows['y'], color='b', label='Real Test')
+ax2.plot(testset.index[w:], gp_y_pred, color='r', label='Predicted Test')
+ax2.set_title('Testset - RMSE = %2.f' % gp_rmse)
+ax2.legend()
+
+plt.show()     
 
 
 ##############################################################################
 # TRAIN THE NETWORK AND PREDICT - With previous y
 ############################################################################## 
 # Train
-trainX = train_windows_gp_inc.values[:,:-1]
-trainY = train_windows_gp_inc.values[:,-1]
-testX = test_windows_gp_inc.values[:,:-1]
-testY = test_windows_gp_inc.values[:,-1]
+trainX = train_windows_y_inc.values[:,:-1]
+trainY = train_windows_y_inc.values[:,-1]
+testX = test_windows_y_inc.values[:,:-1]
+testY = test_windows_y_inc.values[:,-1]
 
-if 'gp_inc' not in locals(): del gp_inc
+if 'gp_inc' in locals(): del gp_inc
 gp_inc = GP(kernel=k, n_restarts_optimizer=9)
 t0 = time.time()
 gp_inc.fit(trainX,trainY)
 tF = time.time()
 
-X = np.concatenate((trainX, testX))
-
 # Predict
-gp_y_pred_inc, gp_y_std_inc = gp_inc.predict(X, return_std=True)
+gp_y_fit_inc = gp_inc.predict(train_windows_y_inc.values[:,:-1], return_std=False)
+gp_y_pred_inc, gp_y_std_inc = gp_inc.predict(test_windows_y_inc.iloc[:,:-1], return_std=True)
 
 gp_y_up_inc = (gp_y_pred_inc + 1.96 * gp_y_std_inc).reshape(-1,)
 gp_y_lw_inc = (gp_y_pred_inc - 1.96 * gp_y_std_inc).reshape(-1,)
@@ -112,12 +148,54 @@ gp_y_pred_inc = gp_y_pred_inc.reshape(-1,1)
 
 
 # Calculating Errors
-gp_residuals_inc = gp_y_pred_inc[len(trainX):] - gp_testset.iloc[5:,-1].values.reshape(-1,1)
+gp_residuals_inc = gp_y_pred_inc - testset.iloc[5:,-1].values.reshape(-1,1)
 gp_rmse_inc = np.sqrt(np.sum(np.power(gp_residuals_inc,2)) / len(gp_residuals_inc))
 print('RMSE = %f' % gp_rmse_inc)
 print('Time to train %.2f' % (tF - t0))
 
 
+# Plot predictions
+f, ax = plt.subplots(1, figsize=(20,6))
+plt.suptitle('Actual vs Predicted - MLR with previous t values' , fontsize=20)
+plt.title('RMSE = %.2f' % gp_rmse_inc, fontsize = 18)
+plt.grid(color='green', linewidth=0.5, alpha=0.5)
+
+plt.scatter(testset.index, testset.y, color='black', s=10)
+plt.plot(testset.index, testset.y, color='b', label='Real Test')
+plt.plot(testset.index[w:], gp_y_pred_inc, color='r', label='Predicted Test')
+
+ax.yaxis.set_major_locator(ticker.MultipleLocator(5))
+ax.yaxis.set_minor_locator(ticker.MultipleLocator(1))
+plt.xlabel('Time')
+plt.ylabel('Response')
+plt.legend()
+plt.show()
+
+
+# Complete Plot
+f, (ax1, ax2) = plt.subplots(2,1, figsize=(20,6))
+plt.suptitle('Actual vs Predicted - Gaussian Process previous Y included' , fontsize=20)
+ax1.grid(color='green', linewidth=0.5, alpha=0.5)
+ax2.grid(color='green', linewidth=0.5, alpha=0.5)
+
+ax1.plot(trainset.index[w:], train_windows['y'], color='b', label='Real Train')
+ax1.plot(trainset.index[w:], gp_y_fit_inc, color='r',
+         linewidth=0.8, alpha=0.8, label='Predicted Train')
+ax1.legend()
+ax1.set_title('Trainset')
+
+ax2.plot(testset.index[w:], test_windows['y'], color='b', label='Real Test')
+ax2.plot(testset.index[w:], gp_y_pred_inc, color='r', label='Predicted Test')
+ax2.set_title('Testset - RMSE = %2.f' % gp_rmse_inc)
+ax2.legend()
+
+plt.show()
+
+
+'''
+##############################################################################
+# COMPARISON BETWEEN BOTH
+############################################################################## 
 # Plot Predictions
 f, (ax1, ax2) = plt.subplots(2, figsize=(20,12))
 plt.suptitle('Actual vs Predicted - Symbolic Regression' , fontsize=20)
@@ -175,4 +253,4 @@ ax2.plot(gp_testset.index[w:], gp_y_pred_inc[len(train_windows):], color='k', la
 ax2.set_title('Testset - RMSE = %2.f' % gp_rmse)
 ax2.legend()
 plt.show()    
-
+'''
